18803e533251cb3b5bf548173d7fab09ce824f53
msegovia-unlam <71356046+msegovia-unlam@users.noreply.github.com>
2022-05-21 23:25:06 -0300

    Add files via upload
    en la parte de configuracion del acceso a la bdd lo tuve que modificar y ponerlo asi:
    
    "AllowedHosts": "*",
      "ConnectionStrings": {
        "DefaultConnection": "Server=DESKTOP-MSCM0UO\\SQLEXPRESS;Database=PeliculasDB;Trusted_Connection=True;MultipleActiveResultSets=true"
      }

---
+++ PeliculaApi/PeliculaApi/Controllers/CategoriaController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PeliculaApi.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeliculaApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriaController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoriaController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCategorias()
+        {
+            var lista = await _db.Categorias.OrderBy(c => c.Nombre).ToListAsync();
+
+            return Ok(lista);
+
+        }
+    }
+}
---
+++ PeliculaApi/PeliculaApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PeliculaApi.Models;
+
+namespace PeliculaApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option) : base(option)
+        {
+
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+    }
+}
